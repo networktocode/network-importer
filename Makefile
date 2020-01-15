3270c0257fd8ed8e06c6d54b2774e4974b03c954
Damien Garros <dgarros@gmail.com>
2020-01-15 09:16:01 -0500

    Increase version to 0.2.1

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 DOCKER_IMAGE = networktocode/network-importer
-DOCKER_VER = 0.1.2-dev
+DOCKER_VER = 0.2.1
 
 format:
 	black --include "bin" .
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-package_version = "0.1.2-dev"
+package_version = "0.2.1"
 package_name = "network-importer"
 
 
