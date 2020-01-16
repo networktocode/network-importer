a6317caa55e79b759711ad0c69e7c913603ecf16
Damien Garros <dgarros@gmail.com>
2020-01-16 06:13:17 -0500

    Fix issue with interface update when interface is L2 but access vlan is not defined anymore on local

--- network_importer/model.py
+++ network_importer/model.py
@@ -256,12 +256,16 @@ class NetworkImporterDevice(object):
                     intf_properties["untagged_vlan"] = self.site.convert_vid_to_nid(
                         intf.local.access_vlan
                     )
+                elif intf.local.mode in ["TRUNK", "ACCESS"] and not intf.local.access_vlan:
+                    intf_properties["untagged_vlan"] = None
 
                 if intf.local.mode == "TRUNK" and intf.local.allowed_vlans:
                     intf_properties["tagged_vlans"] = self.site.convert_vids_to_nids(
                         intf.local.allowed_vlans
                     )
-
+                elif intf.local.mode == "TRUNK" and not intf.local.allowed_vlans:
+                    intf_properties["tagged_vlans"] = []
+                    
             if intf.local.is_lag_member:
                 if intf.local.parent in self.interfaces.keys():
                     if not self.interfaces[intf.local.parent].exist_remote():
