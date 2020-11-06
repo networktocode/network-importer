a39bbc6df96ed85bc958f28858193b368c53e127
Damien Garros <dgarros@gmail.com>
2020-11-06 14:52:54 -0500

    Add unit tests for update_clean_tags

--- tests/unit/adapters/netbox_api/models/test_netbox_vlan_pre29.py
+++ tests/unit/adapters/netbox_api/models/test_netbox_vlan_pre29.py
@@ -18,23 +18,22 @@ import pytest
 import pynetbox
 
 from diffsync.exceptions import ObjectNotFound
-from network_importer.adapters.netbox_api.models import NetboxVlan, NetboxDevice
+from network_importer.adapters.netbox_api.models import NetboxVlanPre29, NetboxDevice
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 FIXTURE_28 = "../fixtures/netbox_28"
-FIXTURE_29 = "../fixtures/netbox_29"
 
 
 def test_vlan_create_from_pynetbox(netbox_api_base):
 
     api = pynetbox.api(url="http://mock", token="1234567890")
 
-    data = yaml.safe_load(open(f"{ROOT}/{FIXTURE_29}/vlan_101_no_tag.json"))
+    data = yaml.safe_load(open(f"{ROOT}/{FIXTURE_28}/vlan_101_no_tag.json"))
     pnb = pynetbox.core.response.Record(values=data, api=api, endpoint=1)
 
-    item = NetboxVlan.create_from_pynetbox(diffsync=netbox_api_base, obj=pnb, site_name="nyc")
+    item = NetboxVlanPre29.create_from_pynetbox(diffsync=netbox_api_base, obj=pnb, site_name="nyc")
 
-    assert isinstance(item, NetboxVlan) is True
+    assert isinstance(item, NetboxVlanPre29) is True
     assert item.remote_id == 1
     assert item.vid == 101
     assert item.associated_devices == []
@@ -44,21 +43,21 @@ def test_vlan_create_from_pynetbox_with_tags(netbox_api_base):
 
     api = pynetbox.api(url="http://mock", token="1234567890")
 
-    data = yaml.safe_load(open(f"{ROOT}/{FIXTURE_29}/vlan_101_tags_01.json"))
+    data = yaml.safe_load(open(f"{ROOT}/{FIXTURE_28}/vlan_101_tags_01.json"))
     pnb = pynetbox.core.response.Record(values=data, api=api, endpoint=1)
 
     netbox_api_base.add(NetboxDevice(name="devA", site_name="nyc", remote_id=30))
 
-    item = NetboxVlan.create_from_pynetbox(diffsync=netbox_api_base, obj=pnb, site_name="nyc")
-    assert isinstance(item, NetboxVlan) is True
+    item = NetboxVlanPre29.create_from_pynetbox(diffsync=netbox_api_base, obj=pnb, site_name="nyc")
+    assert isinstance(item, NetboxVlanPre29) is True
     assert item.remote_id == 1
     assert item.vid == 101
     assert item.associated_devices == ["devA"]
 
     # Try again with one additional device in the inventory
     netbox_api_base.add(NetboxDevice(name="devB", site_name="nyc", remote_id=31))
-    item = NetboxVlan.create_from_pynetbox(diffsync=netbox_api_base, obj=pnb, site_name="nyc")
-    assert isinstance(item, NetboxVlan) is True
+    item = NetboxVlanPre29.create_from_pynetbox(diffsync=netbox_api_base, obj=pnb, site_name="nyc")
+    assert isinstance(item, NetboxVlanPre29) is True
     assert item.remote_id == 1
     assert item.vid == 101
     assert item.associated_devices == ["devA", "devB"]
@@ -66,7 +65,7 @@ def test_vlan_create_from_pynetbox_with_tags(netbox_api_base):
 
 def test_translate_attrs_for_netbox_no_attrs(netbox_api_base):
 
-    vlan = NetboxVlan(vid=100, site_name="HQ", remote_id=30)
+    vlan = NetboxVlanPre29(vid=100, site_name="HQ", remote_id=30)
     netbox_api_base.add(vlan)
 
     params = vlan.translate_attrs_for_netbox({})
@@ -80,11 +79,9 @@ def test_translate_attrs_for_netbox_no_attrs(netbox_api_base):
 
 def test_translate_attrs_for_netbox_with_attrs(netbox_api_base):
 
-    vlan = NetboxVlan(vid=100, site_name="HQ", remote_id=30)
+    vlan = NetboxVlanPre29(vid=100, site_name="HQ", remote_id=30)
     netbox_api_base.add(vlan)
 
-    netbox_api_base.add(NetboxDevice(name="dev1", site_name="HQ", remote_id=32, device_tag_id=12))
-    netbox_api_base.add(NetboxDevice(name="dev2", site_name="HQ", remote_id=33, device_tag_id=13))
     params = vlan.translate_attrs_for_netbox({"name": "VOICE", "associated_devices": ["dev1", "dev2"]})
 
     assert "name" in params
@@ -92,12 +89,12 @@ def test_translate_attrs_for_netbox_with_attrs(netbox_api_base):
     assert "site" in params
     assert params["site"] == 10
     assert "tags" in params
-    assert sorted(params["tags"]) == [12, 13]
+    assert params["tags"] == ["device=dev1", "device=dev2"]
 
 
 def test_translate_attrs_for_netbox_missing_site(netbox_api_base):
 
-    vlan = NetboxVlan(vid=100, site_name="NOTPRESENT", remote_id=30)
+    vlan = NetboxVlanPre29(vid=100, site_name="NOTPRESENT", remote_id=30)
     netbox_api_base.add(vlan)
 
     with pytest.raises(ObjectNotFound):
@@ -107,11 +104,11 @@ def test_translate_attrs_for_netbox_missing_site(netbox_api_base):
 
 def test_update_clean_tags_no_incoming_tags(netbox_api_base):
 
-    vlan = NetboxVlan(vid=100, site_name="HQ", remote_id=30)
+    vlan = NetboxVlanPre29(vid=100, site_name="HQ", remote_id=30)
     netbox_api_base.add(vlan)
 
     api = pynetbox.api(url="http://mock", token="1234567890")
-    data = yaml.safe_load(open(f"{ROOT}/{FIXTURE_29}/vlan_101_tags_01.json"))
+    data = yaml.safe_load(open(f"{ROOT}/{FIXTURE_28}/vlan_101_tags_01.json"))
     pnb = pynetbox.core.response.Record(values=data, api=api, endpoint=1)
 
     params = vlan.translate_attrs_for_netbox({"name": "VOICE"})
@@ -123,18 +120,18 @@ def test_update_clean_tags_no_incoming_tags(netbox_api_base):
 
 def test_update_clean_tags_with_incoming_tags(netbox_api_base):
 
-    vlan = NetboxVlan(vid=100, site_name="HQ", remote_id=30)
+    vlan = NetboxVlanPre29(vid=100, site_name="HQ", remote_id=30)
     netbox_api_base.add(vlan)
 
     netbox_api_base.add(NetboxDevice(name="dev1", site_name="HQ", remote_id=32, device_tag_id=12))
     netbox_api_base.add(NetboxDevice(name="dev2", site_name="HQ", remote_id=33, device_tag_id=13))
 
     api = pynetbox.api(url="http://mock", token="1234567890")
-    data = yaml.safe_load(open(f"{ROOT}/{FIXTURE_29}/vlan_101_tags_01.json"))
+    data = yaml.safe_load(open(f"{ROOT}/{FIXTURE_28}/vlan_101_tags_01.json"))
     pnb = pynetbox.core.response.Record(values=data, api=api, endpoint=1)
 
     params = vlan.translate_attrs_for_netbox({"name": "VOICE", "associated_devices": ["dev1", "dev2"]})
     clean_params = vlan.update_clean_tags(nb_params=params, obj=pnb)
 
     assert "tags" in clean_params
-    assert sorted(clean_params["tags"]) == [1, 2, 3, 12, 13]
+    assert sorted(clean_params["tags"]) == ["device=dev1", "device=dev2", "device=devA", "device=devB", "notadevice"]
