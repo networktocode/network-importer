bbe7448084dee5365d3de4ada7f985c103c3a47a
Damien Garros <dgarros@gmail.com>
2020-12-01 09:57:35 -0500

    Update exception file

--- network_importer/exceptions.py
+++ network_importer/exceptions.py
@@ -14,15 +14,5 @@ limitations under the License.
 """
 
 
-class NetworkImporterException(Exception):
+class AdapterLoadFatalError(Exception):
     """Network Importer Exception."""
-
-    def __init__(self, reason, message, **kwargs):
-        """Initialize exception class."""
-        super(NetworkImporterException, self).__init__(kwargs)
-        self.reason = reason
-        self.message = message
-
-    def __str__(self):
-        """Modified string output."""
-        return f"{self.__class__.__name__}: {self.reason} - {self.message}"
