b2b893698f9c9e3446d52e5d33086111bf762284
Damien Garros <dgarros@gmail.com>
2020-12-01 09:52:38 -0500

    minor cleanup

--- network_importer/adapters/network_importer/exceptions.py
+++ network_importer/adapters/network_importer/exceptions.py
@@ -16,13 +16,3 @@ limitations under the License.
 
 class BatfishObjectNotValid(Exception):
     """Exception to indicate that a given batfish object is not valid."""
-
-    def __init__(self, reason, message, **kwargs):
-        """Initialize exception class."""
-        super(BatfishObjectNotValid, self).__init__(kwargs)
-        self.reason = reason
-        self.message = message
-
-    def __str__(self):
-        """Modified string output."""
-        return f"{self.__class__.__name__}: {self.reason} - {self.message}"
