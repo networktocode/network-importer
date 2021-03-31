c1979dd2a6671a79206342004dc32f4edd222ae1
Rick Donato <rick@donato.me.uk>
2021-03-31 15:26:49 +0100

    Add site vlan to L3 sub interface test

--- tests/unit/adapters/network_importer/test_load_batfish_interface.py
+++ tests/unit/adapters/network_importer/test_load_batfish_interface.py
@@ -154,6 +154,7 @@ def test_load_batfish_interface_intf_ether_sub(network_importer_base, site_sfo, 
     assert not intf.is_lag_member
     assert intf.allowed_vlans == ["sfo__201"]
 
+    assert "sfo__201" in adapter._data['vlan']
 
 def test_load_batfish_interface_intf_lag_member(network_importer_base, site_sfo, dev_spine1):
 
