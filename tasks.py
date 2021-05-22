26d1e07f381f204569fa23b8a0003022277af98f
Josh VanDeraa <jv@networktocode.com>
2021-05-22 15:01:14 -0500

    Style

--- tasks.py
+++ tasks.py
@@ -387,10 +387,10 @@ def compose_nautobot(context, var_envs):
     """
     # Copy the file from tests/docker-compose.test.yml to the tmp directory to be executed from there
     context.run(
-        f"cp  {PWD}/tests/nautobot-docker-compose.test.yml /tmp/docker-compose.yml", pty=True, env=var_envs,
+        f"cp {PWD}/tests/nautobot-docker-compose.test.yml /tmp/docker-compose.yml", pty=True, env=var_envs,
     )
     context.run(
-        f"cp  {PWD}/tests/.creds.tests.env /tmp/.creds.tests.env", pty=True, env=var_envs, 
+        f"cp {PWD}/tests/.creds.tests.env /tmp/.creds.tests.env", pty=True, env=var_envs,
     )
     context.run("cd /tmp && docker-compose pull", pty=True, env=var_envs)
     context.run("cd /tmp && docker-compose up -d", pty=True, env=var_envs)
