6d7ac5dab6985172a7c5d3d4d949ee48d37bed09
Christian Adell <chadell@gmail.com>
2022-06-09 17:51:50 +0200

    Pick version automatically

---
+++ network_importer/__init__.py
@@ -0,0 +1,9 @@
+"Init for network-importer."
+
+try:
+    from importlib import metadata
+except ImportError:
+    # Python version < 3.8
+    import importlib_metadata as metadata
+
+__version__ = metadata.version(__name__)
